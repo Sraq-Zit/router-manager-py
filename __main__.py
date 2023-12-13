57ae1b81a0b3492c770e5dc6c2eb78895ee757aa
Sraq-Zit <kingofmestry@gmail.com>
2023-12-14 00:12:46 +0100

    switched to argparse

--- __main__.py
+++ __main__.py
@@ -1,40 +1,50 @@
-import sys
-
+import argparse, sys
 from routers.flybox import FlyboxRouter
 from utils.network import get_gateway_ip
 
+from utils import settings
 
 def main():
-    if len(sys.argv) != 4:
-        print("Usage: python main.py {info|restart} username password")
-        return
 
-    action = sys.argv[1]
-    username = sys.argv[2]
-    password = sys.argv[3]
+    commands = ['info', 'restart', 'devices']
+
+    parser = argparse.ArgumentParser(description='Interact with routers')
+    parser.add_argument('username', help='Router username')
+    parser.add_argument('password', help='Router password')
+    parser.add_argument('action', choices=commands, help='Action to perform: info, restart, devices')
+    parser.add_argument('extra', nargs='*', default=[], help='Additional arguments for the devices action')
+    parser.add_argument('--as-json', action='store_true', help='Display the information as JSON')
+
+    args = parser.parse_args()
+
+    settings.AS_JSON = args.as_json
 
     gateway = get_gateway_ip()
     if not gateway:
         print("Gateway not found. Please check your network configuration.")
-        sys.exit(1)
+        return 1
 
     routers = [FlyboxRouter]
 
     for r_cls in routers:
-        router = r_cls(username, password)
+        router = r_cls(args.username, args.password)
         if not router.login():
             continue
 
-        if action == "info":
+
+        if args.action == "info":
             print(router.get_router_information())
-        elif action == "restart":
+        elif args.action == "restart":
             router.restart_router()
-        else:
-            print("Invalid action. Supported actions are 'info' and 'restart'.")
+        elif args.action.startswith("devices"):
+            if args.action == "devices":
+                print(router.get_connected_devices().display(not args.extra or args.extra[0] != 'active'))
         
-        return router.logout()
+        router.logout()
+        return 0
 
     print("The current router is not supported/implemented.")
+    return 1
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
